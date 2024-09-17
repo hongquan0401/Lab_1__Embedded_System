e3f9d6727b75ef121e2cab8935fe146bb1291da3
Quan Pham <145032870+hongquan0401@users.noreply.github.com>
2024-09-17 21:01:07 +0700

    toggle heart and angry icon

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+i = 0
+
 def on_forever():
-    basic.show_string("Hello!")
+    global i
+    if i == 0:
+        basic.show_icon(IconNames.ANGRY)
+        i = 1
+    else:
+        basic.show_icon(IconNames.HEART)
+        i = 0
+    basic.pause(500)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let i = 0
 basic.forever(function () {
-    basic.showString("Hello!")
+    if (i == 0) {
+        basic.showIcon(IconNames.Angry)
+        i = 1
+    } else {
+        basic.showIcon(IconNames.Heart)
+        i = 0
+    }
+    basic.pause(500)
 })
