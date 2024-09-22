bb5823595c0f4010b34187f14252ba9d0d293ad6
Quan Pham <145032870+hongquan0401@users.noreply.github.com>
2024-09-22 21:20:15 +0700

    compass 4 direction

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+direction = 0
+
 def on_forever():
-    basic.show_string("Hello!")
+    global direction
+    direction = input.compass_heading()
+    if direction < 45 and direction > 0 or direction >= 315:
+        basic.show_arrow(ArrowNames.NORTH)
+    elif direction >= 45 and direction < 135:
+        basic.show_arrow(ArrowNames.EAST)
+    elif direction >= 135 and direction < 225:
+        basic.show_arrow(ArrowNames.SOUTH)
+    else:
+        basic.show_arrow(ArrowNames.WEST)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-    basic.showString("Hello!")
+let direction = 0
+basic.forever(function on_forever() {
+    
+    direction = input.compassHeading()
+    if (direction < 45 && direction > 0 || direction >= 315) {
+        basic.showArrow(ArrowNames.North)
+    } else if (direction >= 45 && direction < 135) {
+        basic.showArrow(ArrowNames.East)
+    } else if (direction >= 135 && direction < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else {
+        basic.showArrow(ArrowNames.West)
+    }
+    
 })
