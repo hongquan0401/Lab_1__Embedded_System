3194cee834247a09c062ee6b8918d93fe97e3f89
Quan Pham <145032870+hongquan0401@users.noreply.github.com>
2024-09-22 20:40:15 +0700

    done

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+def on_gesture_logo_up():
+    basic.show_number(1)
+    basic.pause(200)
+    basic.clear_screen()
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
+
+def on_gesture_tilt_left():
+    basic.show_number(3)
+    basic.pause(200)
+    basic.clear_screen()
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def on_gesture_tilt_right():
+    basic.show_number(4)
+    basic.pause(200)
+    basic.clear_screen()
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
+def on_gesture_logo_down():
+    basic.show_number(2)
+    basic.pause(200)
+    basic.clear_screen()
+input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
+
 def on_forever():
-    basic.show_string("Hello!")
+    pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showNumber(1)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showNumber(3)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showNumber(4)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showNumber(2)
+    basic.pause(200)
+    basic.clearScreen()
+})
 basic.forever(function () {
-    basic.showString("Hello!")
+	
 })
