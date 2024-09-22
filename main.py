bc199be03d58e39b2e0833e83a82b947aa48eea4
Quan Pham <145032870+hongquan0401@users.noreply.github.com>
2024-09-22 21:58:00 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+def on_received_number(receivedNumber):
+    if receivedNumber == 0:
+        basic.show_arrow(ArrowNames.NORTH)
+    else:
+        basic.show_arrow(ArrowNames.SOUTH)
+    basic.pause(100)
+    basic.clear_screen()
+radio.on_received_number(on_received_number)
+
+def on_gesture_logo_up():
+    radio.send_number(0)
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
+
+def on_gesture_logo_down():
+    radio.send_number(1)
+input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
+
+radio.set_group(1)
+
 def on_forever():
-    basic.show_string("Hello!")
+    if input.temperature() >= 30:
+        led.plot(0, 0)
+    else:
+        led.unplot(0, 0)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else {
+        basic.showArrow(ArrowNames.South)
+    }
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(0)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
 basic.forever(function () {
-    basic.showString("Hello!")
+    if (input.temperature() >= 30) {
+        led.plot(0, 0)
+    } else {
+        led.unplot(0, 0)
+    }
 })
