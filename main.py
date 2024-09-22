300463e2e104aadb084bcee6889e05f11a4bfe84
Quan Pham <145032870+hongquan0401@users.noreply.github.com>
2024-09-22 17:41:17 +0700

    use cmd show_icon to display my love to u (heart)

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ i = 0
 def on_forever():
     global i
     if i == 0:
-        basic.show_icon(IconNames.ANGRY)
-        i = 1
-    else:
         basic.show_icon(IconNames.HEART)
-        i = 0
+        i = i + 1
+    else:
+        basic.show_icon(IconNames.SMALL_HEART)
     basic.pause(500)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 let i = 0
 basic.forever(function () {
     if (i == 0) {
-        basic.showIcon(IconNames.Angry)
-        i = 1
-    } else {
         basic.showIcon(IconNames.Heart)
-        i = 0
+        i = i + 1
+    } else {
+        basic.showIcon(IconNames.SmallHeart)
     }
     basic.pause(500)
 })
