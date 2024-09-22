37c7802e430be25a3c14c36e8a35d2726f6c6f61
Quan Pham <145032870+hongquan0401@users.noreply.github.com>
2024-09-22 18:04:24 +0700

    display my luv for 2s and diamond after that

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+counter = 0
+
 def on_forever():
-    basic.show_string("Hello!")
+    global counter
+    if counter < 2:
+        basic.show_icon(IconNames.HEART)
+        counter = counter + 1
+    elif counter % 2 == 0:
+        basic.show_icon(IconNames.SMALL_DIAMOND)
+        counter = counter + 1
+    else:
+        basic.show_icon(IconNames.DIAMOND)
+        counter = counter + 1
+    basic.pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-    basic.showString("Hello!")
+let counter = 0
+basic.forever(function on_forever() {
+    
+    if (counter < 2) {
+        basic.showIcon(IconNames.Heart)
+        counter = counter + 1
+    } else if (counter % 2 == 0) {
+        basic.showIcon(IconNames.SmallDiamond)
+        counter = counter + 1
+    } else {
+        basic.showIcon(IconNames.Diamond)
+        counter = counter + 1
+    }
+    
+    basic.pause(1000)
 })
