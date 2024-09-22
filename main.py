ef7a316d299b190b31b6262754dc78660c9d2fe7
Quan Pham <145032870+hongquan0401@users.noreply.github.com>
2024-09-22 20:48:35 +0700

    done

--- main.py
+++ main.py
@@ -1,3 +1,109 @@
+def on_gesture_tilt_left():
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        . . . # .
+        . . . . #
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . #
+        . . . # .
+        . . # . .
+        . . . # .
+        . . . . #
+        """)
+    basic.show_leds("""
+        . . . # .
+        . . # . .
+        . # . . .
+        . . # . .
+        . . . # .
+        """)
+    basic.show_leds("""
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        """)
+    basic.show_leds("""
+        . # . . .
+        # . . . .
+        . . . . .
+        # . . . .
+        . # . . .
+        """)
+    basic.show_leds("""
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        """)
+    basic.clear_screen()
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def on_gesture_tilt_right():
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        # . . . .
+        . # . . .
+        . . # . .
+        . # . . .
+        # . . . .
+        """)
+    basic.show_leds("""
+        . # . . .
+        . . # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        """)
+    basic.show_leds("""
+        . . # . .
+        . . . # .
+        . . . . #
+        . . . # .
+        . . # . .
+        """)
+    basic.show_leds("""
+        . . . # .
+        . . . . #
+        . . . . .
+        . . . . #
+        . . . # .
+        """)
+    basic.show_leds("""
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.clear_screen()
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
 def on_forever():
-    basic.show_string("Hello!")
+    pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,107 @@
-basic.forever(function () {
-    basic.showString("Hello!")
+input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . # . . .
+        . . # . .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        . . . . .
+        # . . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        `)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . . . #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . . . . .
+        . . . . #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.clearScreen()
+})
+basic.forever(function on_forever() {
+    
 })
